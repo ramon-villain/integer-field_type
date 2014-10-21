48616020839f151a3f1356f8e02a884a90ccea39
Ryan Thompson <service@aiwebsystems.com>
2014-10-21 08:41:29 -0500

    Fixing

--- src/IntegerFieldType.php
+++ src/IntegerFieldType.php
@@ -1,8 +1,8 @@
-<?php namespace Anomaly\Streams\FieldType\Integer;
+<?php namespace Anomaly\Streams\Addon\FieldType\Integer;
 
-use Streams\Core\Addon\FieldTypeAbstract;
+use Anomaly\Streams\Platform\Addon\FieldType\FieldTypeAddon;
 
-class IntegerFieldType extends FieldTypeAbstract
+class IntegerFieldType extends FieldTypeAddon
 {
     /**
      * The database column type this field type uses.
