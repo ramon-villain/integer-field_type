3d7ea70387025168a4009d42e60e17a7a735e348
Ryan Thompson <service@aiwebsystems.com>
2014-10-22 13:35:12 -0500

    Clean before final

--- src/IntegerFieldType.php
+++ src/IntegerFieldType.php
@@ -4,33 +4,10 @@ use Anomaly\Streams\Platform\Addon\FieldType\FieldTypeAddon;
 
 class IntegerFieldType extends FieldTypeAddon
 {
-    protected $slug = 'integer';
-
-    /**
-     * The database column type this field type uses.
-     *
-     * @var string
-     */
     public $columnType = 'integer';
 
-    /**
-     * Return the input used for forms.
-     *
-     * @return mixed
-     */
     public function input()
     {
         return \Form::input('number', $this->inputName(), $this->value);
-    }
-
-    /**
-     * Force an integer value.
-     *
-     * @param $value
-     * @return int|mixed
-     */
-    public function mutate($value)
-    {
-        return (int)$value;
     }
 }
