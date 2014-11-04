aaf3fa080826cacb9aa8c538e20963f6f08efa27
Ryan Thompson <service@aiwebsystems.com>
2014-11-04 00:45:25 -0600

    Doc'd

--- src/IntegerFieldType.php
+++ src/IntegerFieldType.php
@@ -2,12 +2,50 @@
 
 use Anomaly\Streams\Platform\Addon\FieldType\FieldTypeAddon;
 
+/**
+ * Class IntegerFieldType
+ *
+ * @link          http://anomaly.is/streams-platform
+ * @author        AnomalyLabs, Inc. <hello@anomaly.is>
+ * @author        Ryan Thompson <ryan@anomaly.is>
+ * @package       Anomaly\Streams\Addon\FieldType\Integer
+ */
 class IntegerFieldType extends FieldTypeAddon
 {
-    public $columnType = 'integer';
 
+    /**
+     * Base field type rules.
+     *
+     * @var array
+     */
+    protected $rules = ['integer'];
+
+    /**
+     * Uses integer database column.
+     *
+     * @var string
+     */
+    protected $columnType = 'integer';
+
+    /**
+     * Return the input HTML.
+     *
+     * @return mixed
+     */
     public function input()
     {
-        return \Form::input('number', $this->inputName(), $this->value);
+        return app('form')->input('number', $this->getFieldName(), $this->getValue());
+    }
+
+    /**
+     * Hook into the setAttribute and make
+     * sure the value is an integer.
+     *
+     * @param $value
+     * @return int
+     */
+    public function onSet($value)
+    {
+        return (int)$value;
     }
 }
